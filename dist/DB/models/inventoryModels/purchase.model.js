f72d15e11bb2fe22cf93cc09a291eaf72707c317
imohamedfekry <mhamedfekry0@gmail.com>
2025-07-21 20:19:57 +0300

    auto update

--- dist/DB/models/inventoryModels/purchase.model.js
+++ dist/DB/models/inventoryModels/purchase.model.js
@@ -21,13 +21,23 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
@@ -161,15 +171,6 @@ exports.Purchase = Purchase = __decorate([
     })
 ], Purchase);
 exports.PurchaseSchema = mongoose_1.SchemaFactory.createForClass(Purchase);
-// Indexes for better performance
-exports.PurchaseSchema.index({ purchaseNumber: 1 });
-exports.PurchaseSchema.index({ purchaseOrderId: 1 });
-exports.PurchaseSchema.index({ supplierId: 1 });
-exports.PurchaseSchema.index({ branchId: 1 });
-exports.PurchaseSchema.index({ status: 1 });
-exports.PurchaseSchema.index({ purchaseDate: -1 });
-exports.PurchaseSchema.index({ isDeleted: 1 });
-exports.PurchaseSchema.index({ createdAt: -1 });
 exports.PURCHASE_MODEL = 'PURCHASE_MODEL';
 exports.PurchaseModel = mongoose_1.MongooseModule.forFeature([
     { name: 'Purchase', schema: exports.PurchaseSchema }
