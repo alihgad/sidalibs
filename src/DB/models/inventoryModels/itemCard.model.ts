520091635a88d61e087406f6749f10038313dc9b
alihgad <alihgad2@gmail.com>
2025-09-13 21:05:46 +0300

    itemCardCalculating

--- src/DB/models/inventoryModels/itemCard.model.ts
+++ src/DB/models/inventoryModels/itemCard.model.ts
@@ -19,7 +19,9 @@ export class materialCard {
     startPrice:number,
     oldInputs:Transaction[],
     oldOutputs:Transaction[],
-    branchId:Types.ObjectId
+    branchId:Types.ObjectId,
+    totalQuantity:number,
+    totalAmount:number
     ) {
         this.materialId = materialId
         this.branchId = branchId
@@ -30,6 +32,8 @@ export class materialCard {
         this.startPrice = startPrice
         this.oldInputs = oldInputs
         this.oldOutputs = oldOutputs
+        this.totalQuantity = totalQuantity
+        this.totalAmount = totalAmount 
     }
     @Prop({ type: Types.ObjectId  , ref : "Materials"})
     materialId: Types.ObjectId;
@@ -58,10 +62,35 @@ export class materialCard {
     @Prop({ type: Number , default:0}) 
     startPrice : number
 
+    @Prop({ type: Number , default:0})
+    totalQuantity : number
+
+    @Prop({ type: Number , default:0})
+    totalAmount : number
+
 }
 
 export type materialCardDocument = HydratedDocument<materialCard>;
 export const materialCardSchema = SchemaFactory.createForClass(materialCard);
+let calculating = (doc : materialCard)=>{
+    doc.inputs.forEach(input => {
+        doc.totalQuantity += input.quantity
+        doc.totalAmount += input.amount
+    })
+
+    doc.price = doc.totalAmount / doc.totalQuantity
+
+}
+// Hook واحد لجميع عمليات البحث
+materialCardSchema.pre(/^find/, function() {
+    calculating(this as unknown as materialCard)
+})
+
+materialCardSchema.pre("updateOne", function() {
+    throw new Error("not allowed use findOneAndUpdate")
+})
+
+
 
 export const material_CARD_MODEL = 'material_CARD_MODEL';
 export const materialCardModel = MongooseModule.forFeature([
